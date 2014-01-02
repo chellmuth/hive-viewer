51713e36c050bec0e55aa33ae919458ad569e7bc
Chris Hellmuth <chellmuth@gmail.com>
2014-01-02 01:51:00 -0700

    Begin hex_math work in preparation of hit detection on web canvas

---
+++ lib/hex_math.dart
@@ -0,0 +1,84 @@
+library hex_math;
+
+import 'dart:math';
+
+import 'gamemodel.dart';
+
+class Hexmap {
+  num width;
+  num height;
+
+  // fraction of height of one point
+  num pointHeight;
+
+  Hexmap(this.width, this.height, this.pointHeight);
+}
+
+Coordinate hexAtPoint(Hexmap hexmap, Point point) {
+
+}
+
+List<Coordinate> hexRing(int radius) {
+  var hexes = new List<Coordinate>();
+
+  // start right, then move counter-clockwise around
+  hexes.add(new Coordinate(0, radius));
+
+  for (var i = 0; i < radius; i++) {
+    hexes.add(hexes.last.applyDirection(Direction.UP_LEFT));
+  }
+  for (var i = 0; i < radius; i++) {
+    hexes.add(hexes.last.applyDirection(Direction.LEFT));
+  }
+  for (var i = 0; i < radius; i++) {
+    hexes.add(hexes.last.applyDirection(Direction.DOWN_LEFT));
+  }
+  for (var i = 0; i < radius; i++) {
+    hexes.add(hexes.last.applyDirection(Direction.DOWN_RIGHT));
+  }
+  for (var i = 0; i < radius; i++) {
+    hexes.add(hexes.last.applyDirection(Direction.RIGHT));
+  }
+  // note i < radius - 1 constraint to avoid re-adding initial coordinate
+  for (var i = 0; i < radius - 1; i++) {
+    hexes.add(hexes.last.applyDirection(Direction.UP_RIGHT));
+  }
+
+  return hexes;
+}
+
+bool _checkPointInTriangle(Point point, Point t0, Point t1, Point t2) {
+  num compute_area(t0, t1, t2) {
+    return 1 / 2 * (-t1.y * t2.x + t0.y * (-t1.x + t2.x) + t0.x * (t1.y - t2.y) + t1.x * t2.y);
+  }
+
+  var area = compute_area(t0, t1, t2);
+
+  num s = 1 / (2 * area) * (t0.y * t2.x - t0.x * t2.y + (t2.y - t0.y) * point.x + (t0.x - t2.x) * point.y);
+  num t = 1 / (2 * area) * (t0.x * t1.y - t0.y * t1.x + (t0.y - t1.y) * point.x + (t1.x - t0.x) * point.y);
+
+  return s > 0 && t > 0 && 1 - s - t > 0;
+}
+
+bool checkHex(Hexmap hexmap, Coordinate hex, Point point) {
+  num xOffset = hex.row % 2 == 0 ? 0 : hexmap.width * .5;
+  Rectangle hexRect = new Rectangle(
+      xOffset + hexmap.width * hex.col,
+      hexmap.height * (.75 * hex.row + hexmap.pointHeight),
+      hexmap.width,
+      hexmap.height * (1 - 2 * hexmap.pointHeight)
+  );
+  if (hexRect.containsPoint(point)) { return true; }
+
+  Point upperLeft = new Point(hexRect.left, hexRect.top);
+  Point upperMiddle = new Point(hexRect.left + hexmap.width * .5, hexRect.top - hexmap.height * hexmap.pointHeight);
+  Point upperRight = new Point(hexRect.right, hexRect.top);
+  if (_checkPointInTriangle(point, upperLeft, upperMiddle, upperRight)) { return true; }
+
+  Point lowerLeft = new Point(hexRect.left, hexRect.bottom);
+  Point lowerMiddle = new Point(hexRect.left + hexmap.width * .5, hexRect.bottom + hexmap.height * hexmap.pointHeight);
+  Point lowerRight = new Point(hexRect.right, hexRect.bottom);
+  if (_checkPointInTriangle(point, lowerLeft, lowerMiddle, lowerRight)) { return true; }
+
+  return false;
+}
---
+++ test/hex_math_test.dart
@@ -0,0 +1,100 @@
+library gamemodel_test;
+
+import 'dart:math';
+
+import 'package:unittest/unittest.dart';
+
+import '../lib/hex_math.dart';
+import '../lib/gamemodel.dart';
+
+main() {
+  group('check hex', () {
+    var width = 10;
+    var height = 40;
+    var hexmap = new Hexmap(width, height, .25);
+    group('even', () {
+      var row = 2, col = 4;
+      test('hit upper triangle', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 1)), isTrue);
+      });
+      test('hit mid box', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 15)), isTrue);
+      });
+      test('hit lower triangle', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 39)), isTrue);
+      });
+      test('miss up left', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col, .75 * height * row + 1)), isFalse);
+      });
+      test('miss up right', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * (col + 1), .75 * height * row + 1)), isFalse);
+      });
+      test('miss down left', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col, .75 * height * row + 39)), isFalse);
+      });
+      test('miss down right', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 1, .75 * height * row + 39)), isFalse);
+      });
+    });
+
+    group('odd', () {
+      var row = 3, col = 4;
+      var xOffset = .5 * width;
+      test('hit upper triangle', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 1)), isTrue);
+      });
+      test('hit mid box', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 15)), isTrue);
+      });
+      test('hit lower triangle', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 39)), isTrue);
+      });
+      test('miss up left', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col, .75 * height * row + 1)), isFalse);
+      });
+      test('miss up right', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * (col + 1), .75 * height * row + 1)), isFalse);
+      });
+      test('miss down left', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col, .75 * height * row + 39)), isFalse);
+      });
+      test('miss down right', () {
+        expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 1, .75 * height * row + 39)), isFalse);
+      });
+    });
+  });
+  
+  group('hex ring', () {
+    test('radius 0', () {
+      expect(hexRing(0), equals([ new Coordinate(0, 0) ]));
+    });
+    test('radius 1', () {
+      var coordinates = [
+        new Coordinate(0, 1),
+        new Coordinate(-1, 0),
+        new Coordinate(-1, -1),
+        new Coordinate(0, -1),
+        new Coordinate(1, -1),
+        new Coordinate(1, 0)
+      ];
+      expect(hexRing(1), equals(coordinates));
+    });
+    test('radius 2', () {
+      var coordinates = [
+        new Coordinate(0, 2),
+        new Coordinate(-1, 1),
+        new Coordinate(-2, 1),
+        new Coordinate(-2, 0),
+        new Coordinate(-2, -1),
+        new Coordinate(-1, -2),
+        new Coordinate(0, -2),
+        new Coordinate(1, -2),
+        new Coordinate(2, -1),
+        new Coordinate(2, 0),
+        new Coordinate(2, 1),
+        new Coordinate(1, 1)
+      ];
+      expect(hexRing(2), equals(coordinates));
+    });
+  });
+}
