eb3ae7fa03e569f6fa601b5ab32c1eacc62199a7
Chris Hellmuth <chellmuth@gmail.com>
2014-01-01 22:35:48 -0700

    Minor cleanup for RangedSlideMoveFinder.findMoves

--- lib/move_finder.dart
+++ lib/move_finder.dart
@@ -32,38 +32,55 @@ class JumpMoveFinder {
 }
 
 class RangedSlideMoveFinder {
-  static List<Move> findMoves(int range, Piece piece, GameState gamestate) {
-    var startingLocation = gamestate.locate(piece);
+  static List<List<Coordinate>> _extendTransitionList(List<Coordinate> transitionList, GameState gamestate) {
+    var transitionLists = new List<List<Coordinate>>();
+    Coordinate lastTransition = transitionList.last;
+    for (Tile neighbor in gamestate.neighbors(lastTransition)) {
+      var neighborDirection = lastTransition.direction(neighbor.coordinate);
+      for (var transitionDirection in neighborDirection.adjacentDirections()) {
+        var newLocation = lastTransition.applyDirection(transitionDirection);
+        if (gamestate.isLocationEmpty(newLocation)) {
+          var newTransitionList = new List<Coordinate>.from(transitionList);
+          newTransitionList.add(newLocation);
+          transitionLists.add(newTransitionList);
+        }
+      }
+    }
+    return transitionLists;
+  }
+  
+  static List<List<Coordinate>> _buildTransitionLists(int range, Piece piece, GameState gamestate) {
     gamestate = gamestate.copy();
-
-    var finalLocations = new Set<Coordinate>();
 
     var transitionLists = [ [ gamestate.locate(piece) ] ];
     gamestate.toList().removeWhere((tile) => tile.piece == piece);
+
     for (int i = 0; i < range; i++) {
       var nextTransitionLists = [];
       for (List<Coordinate> transitionList in transitionLists) {
-        Coordinate lastTransition = transitionList.last;
-        for (Tile neighbor in gamestate.neighbors(lastTransition)) {
-          var neighborDirection = lastTransition.direction(neighbor.coordinate);
-          for (var transitionDirection in neighborDirection.adjacentDirections()) {
-            var newLocation = lastTransition.applyDirection(transitionDirection);
-            if (gamestate.isLocationEmpty(newLocation)) {
-              var newTransitionList = new List<Coordinate>.from(transitionList);
-              newTransitionList.add(newLocation);
-              nextTransitionLists.add(newTransitionList);
-            }
-          }
-        }
+        nextTransitionLists.addAll(_extendTransitionList(transitionList, gamestate));
       }
       transitionLists = nextTransitionLists;
     }
+    return transitionLists;
+  }
 
+  static Set<Coordinate> _buildMoveLocationsFromTransitionLists(int range, List<List<Coordinate>> transitionLists) {
+    var moveLocations = new Set<Coordinate>();
     for (List<Coordinate> transition in transitionLists) {
       if (new Set<Coordinate>.from(transition).length == range + 1) {
-        finalLocations.add(transition.last);
+        moveLocations.add(transition.last);
       }
     }
-    return finalLocations.map((location) => new Move(piece, startingLocation, location)).toList();    
+    return moveLocations;
+  }
+
+  static List<Move> findMoves(int range, Piece piece, GameState gamestate) {
+    var startingLocation = gamestate.locate(piece);
+
+    List<List<Coordinate>> transitionLists = _buildTransitionLists(range, piece, gamestate); 
+    Set<Coordinate> moveLocations = _buildMoveLocationsFromTransitionLists(range, transitionLists);
+
+    return moveLocations.map((location) => new Move(piece, startingLocation, location)).toList();    
   } 
 }
