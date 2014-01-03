f6fd9a41652a2de8f5178c0124a7cbed7903fbd8
Chris Hellmuth <chellmuth@gmail.com>
2014-01-02 18:47:23 -0700

    Add group prefixes to newly harnessed console tests

--- test/gamemodel_test.dart
+++ test/gamemodel_test.dart
@@ -2,38 +2,40 @@ part of hive_console_test;
 
 class TestGameModel {
   static void run() {
-    test('Player equality', () {
-      expect(Player.WHITE, equals(Player.WHITE));
-      expect(Player.WHITE, isNot(equals(Player.BLACK)));
+    group('Game model:', () {
+      test('Player equality', () {
+        expect(Player.WHITE, equals(Player.WHITE));
+        expect(Player.WHITE, isNot(equals(Player.BLACK)));
+      });
+
+      test('Bug equality', () {
+        expect(Bug.ANT, equals(Bug.ANT));
+        expect(Bug.ANT, isNot(equals(Bug.QUEEN)));
+      });
+
+      test('Direction equality', () {
+        expect(Direction.LEFT, equals(Direction.LEFT));
+        expect(Direction.LEFT, isNot(equals(Direction.RIGHT)));
+      });
+
+      test('Piece equality', () {
+        expect(new Piece(Player.WHITE, Bug.ANT, 1), equals(new Piece(Player.WHITE, Bug.ANT, 1)));
+
+        expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.BLACK, Bug.ANT, 1))));
+        expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.WHITE, Bug.QUEEN, 1))));
+        expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.WHITE, Bug.ANT, 0))));
+      });
+
+      test('Piece hashCode', () {
+        expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, equals(new Piece(Player.WHITE, Bug.ANT, 1).hashCode));
+
+        expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.BLACK, Bug.ANT, 1).hashCode)));
+        expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.WHITE, Bug.QUEEN, 1).hashCode)));
+        expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.WHITE, Bug.ANT, 0).hashCode)));
+      });
+
+      group('Coordinate', _coordinate);
     });
-
-    test('Bug equality', () {
-      expect(Bug.ANT, equals(Bug.ANT));
-      expect(Bug.ANT, isNot(equals(Bug.QUEEN)));
-    });
-
-    test('Direction equality', () {
-      expect(Direction.LEFT, equals(Direction.LEFT));
-      expect(Direction.LEFT, isNot(equals(Direction.RIGHT)));
-    });
-
-    test('Piece equality', () {
-      expect(new Piece(Player.WHITE, Bug.ANT, 1), equals(new Piece(Player.WHITE, Bug.ANT, 1)));
-
-      expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.BLACK, Bug.ANT, 1))));
-      expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.WHITE, Bug.QUEEN, 1))));
-      expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.WHITE, Bug.ANT, 0))));
-    });
-
-    test('Piece hashCode', () {
-      expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, equals(new Piece(Player.WHITE, Bug.ANT, 1).hashCode));
-
-      expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.BLACK, Bug.ANT, 1).hashCode)));
-      expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.WHITE, Bug.QUEEN, 1).hashCode)));
-      expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.WHITE, Bug.ANT, 0).hashCode)));
-    });
-
-    group('Coordinate', _coordinate);
   }
 
   static void _coordinate() {
--- test/hex_math_test.dart
+++ test/hex_math_test.dart
@@ -2,93 +2,95 @@ part of hive_console_test;
 
 class TestHexMath {
   static void run() {
-    group('check hex', () {
-      var width = 10;
-      var height = 40;
-      var hexmap = new Hexmap(width, height, .25);
-      group('even', () {
-        var row = 2, col = 4;
-        test('hit upper triangle', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 1)), isTrue);
+    group('Hex math:', () {
+      group('check hex', () {
+        var width = 10;
+        var height = 40;
+        var hexmap = new Hexmap(width, height, .25);
+        group('even', () {
+          var row = 2, col = 4;
+          test('hit upper triangle', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 1)), isTrue);
+          });
+          test('hit mid box', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 15)), isTrue);
+          });
+          test('hit lower triangle', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 39)), isTrue);
+          });
+          test('miss up left', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col, .75 * height * row + 1)), isFalse);
+          });
+          test('miss up right', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * (col + 1), .75 * height * row + 1)), isFalse);
+          });
+          test('miss down left', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col, .75 * height * row + 39)), isFalse);
+          });
+          test('miss down right', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 1, .75 * height * row + 39)), isFalse);
+          });
         });
-        test('hit mid box', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 15)), isTrue);
-        });
-        test('hit lower triangle', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 5, .75 * height * row + 39)), isTrue);
-        });
-        test('miss up left', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col, .75 * height * row + 1)), isFalse);
-        });
-        test('miss up right', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * (col + 1), .75 * height * row + 1)), isFalse);
-        });
-        test('miss down left', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col, .75 * height * row + 39)), isFalse);
-        });
-        test('miss down right', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(width * col + 1, .75 * height * row + 39)), isFalse);
+
+        group('odd', () {
+          var row = 3, col = 4;
+          var xOffset = .5 * width;
+          test('hit upper triangle', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 1)), isTrue);
+          });
+          test('hit mid box', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 15)), isTrue);
+          });
+          test('hit lower triangle', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 39)), isTrue);
+          });
+          test('miss up left', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col, .75 * height * row + 1)), isFalse);
+          });
+          test('miss up right', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * (col + 1), .75 * height * row + 1)), isFalse);
+          });
+          test('miss down left', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col, .75 * height * row + 39)), isFalse);
+          });
+          test('miss down right', () {
+            expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 1, .75 * height * row + 39)), isFalse);
+          });
         });
       });
 
-      group('odd', () {
-        var row = 3, col = 4;
-        var xOffset = .5 * width;
-        test('hit upper triangle', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 1)), isTrue);
+      group('hex ring', () {
+        test('radius 0', () {
+          expect(hexRing(0), equals([ new Coordinate(0, 0) ]));
         });
-        test('hit mid box', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 15)), isTrue);
+        test('radius 1', () {
+          var coordinates = [
+            new Coordinate(0, 1),
+            new Coordinate(-1, 0),
+            new Coordinate(-1, -1),
+            new Coordinate(0, -1),
+            new Coordinate(1, -1),
+            new Coordinate(1, 0)
+          ];
+          expect(hexRing(1), equals(coordinates));
         });
-        test('hit lower triangle', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 5, .75 * height * row + 39)), isTrue);
+        test('radius 2', () {
+          var coordinates = [
+            new Coordinate(0, 2),
+            new Coordinate(-1, 1),
+            new Coordinate(-2, 1),
+            new Coordinate(-2, 0),
+            new Coordinate(-2, -1),
+            new Coordinate(-1, -2),
+            new Coordinate(0, -2),
+            new Coordinate(1, -2),
+            new Coordinate(2, -1),
+            new Coordinate(2, 0),
+            new Coordinate(2, 1),
+            new Coordinate(1, 1)
+          ];
+          expect(hexRing(2), equals(coordinates));
         });
-        test('miss up left', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col, .75 * height * row + 1)), isFalse);
-        });
-        test('miss up right', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * (col + 1), .75 * height * row + 1)), isFalse);
-        });
-        test('miss down left', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col, .75 * height * row + 39)), isFalse);
-        });
-        test('miss down right', () {
-          expect(checkHex(hexmap, new Coordinate(row, col), new Point(xOffset + width * col + 1, .75 * height * row + 39)), isFalse);
-        });
-      });
-    });
-
-    group('hex ring', () {
-      test('radius 0', () {
-        expect(hexRing(0), equals([ new Coordinate(0, 0) ]));
-      });
-      test('radius 1', () {
-        var coordinates = [
-          new Coordinate(0, 1),
-          new Coordinate(-1, 0),
-          new Coordinate(-1, -1),
-          new Coordinate(0, -1),
-          new Coordinate(1, -1),
-          new Coordinate(1, 0)
-        ];
-        expect(hexRing(1), equals(coordinates));
-      });
-      test('radius 2', () {
-        var coordinates = [
-          new Coordinate(0, 2),
-          new Coordinate(-1, 1),
-          new Coordinate(-2, 1),
-          new Coordinate(-2, 0),
-          new Coordinate(-2, -1),
-          new Coordinate(-1, -2),
-          new Coordinate(0, -2),
-          new Coordinate(1, -2),
-          new Coordinate(2, -1),
-          new Coordinate(2, 0),
-          new Coordinate(2, 1),
-          new Coordinate(1, 1)
-        ];
-        expect(hexRing(2), equals(coordinates));
       });
     });
   }
