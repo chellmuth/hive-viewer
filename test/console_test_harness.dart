b0f116a3a402ea4d17ae8b4dd67d69c41d98c06c
Chris Hellmuth <chellmuth@gmail.com>
2014-01-02 18:45:09 -0700

    Move existing console tests into console_test_harness

--- test/console_test_harness.dart
+++ test/console_test_harness.dart
@@ -2,17 +2,25 @@ library hive_console_test;
 
 import 'package:unittest/unittest.dart';  
 
+import 'dart:math';
+
 import '../lib/rules.dart';
 import '../lib/gamemodel.dart';
 import '../lib/gamestate.dart';
 import '../lib/move_finder.dart';
+import '../lib/hex_math.dart';
+
 
 part 'rules_test.dart';
 part 'gamestate_test.dart';
 part 'move_finder_test.dart';
+part 'hex_math_test.dart';
+part 'gamemodel_test.dart';
 
 main() {
   TestRules.run();
   TestGameState.run();
   TestMoveFinder.run();
+  TestHexMath.run();
+  TestGameModel.run();
 }
--- test/gamemodel_test.dart
+++ test/gamemodel_test.dart
@@ -1,109 +1,107 @@
-library gamemodel_test;
+part of hive_console_test;
 
-import 'package:unittest/unittest.dart';  
+class TestGameModel {
+  static void run() {
+    test('Player equality', () {
+      expect(Player.WHITE, equals(Player.WHITE));
+      expect(Player.WHITE, isNot(equals(Player.BLACK)));
+    });
 
-import '../lib/gamemodel.dart';
+    test('Bug equality', () {
+      expect(Bug.ANT, equals(Bug.ANT));
+      expect(Bug.ANT, isNot(equals(Bug.QUEEN)));
+    });
 
-main() {
-  test('Player equality', () {
-    expect(Player.WHITE, equals(Player.WHITE));
-    expect(Player.WHITE, isNot(equals(Player.BLACK)));
-  });
-  
-  test('Bug equality', () {
-    expect(Bug.ANT, equals(Bug.ANT));
-    expect(Bug.ANT, isNot(equals(Bug.QUEEN)));
-  });
+    test('Direction equality', () {
+      expect(Direction.LEFT, equals(Direction.LEFT));
+      expect(Direction.LEFT, isNot(equals(Direction.RIGHT)));
+    });
 
-  test('Direction equality', () {
-    expect(Direction.LEFT, equals(Direction.LEFT));
-    expect(Direction.LEFT, isNot(equals(Direction.RIGHT)));
-  });
+    test('Piece equality', () {
+      expect(new Piece(Player.WHITE, Bug.ANT, 1), equals(new Piece(Player.WHITE, Bug.ANT, 1)));
 
-  test('Piece equality', () {
-    expect(new Piece(Player.WHITE, Bug.ANT, 1), equals(new Piece(Player.WHITE, Bug.ANT, 1)));
+      expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.BLACK, Bug.ANT, 1))));
+      expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.WHITE, Bug.QUEEN, 1))));
+      expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.WHITE, Bug.ANT, 0))));
+    });
 
-    expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.BLACK, Bug.ANT, 1))));
-    expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.WHITE, Bug.QUEEN, 1))));
-    expect(new Piece(Player.WHITE, Bug.ANT, 1), isNot(equals(new Piece(Player.WHITE, Bug.ANT, 0))));
-  });
-  
-  test('Piece hashCode', () {
-    expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, equals(new Piece(Player.WHITE, Bug.ANT, 1).hashCode));
+    test('Piece hashCode', () {
+      expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, equals(new Piece(Player.WHITE, Bug.ANT, 1).hashCode));
 
-    expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.BLACK, Bug.ANT, 1).hashCode)));
-    expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.WHITE, Bug.QUEEN, 1).hashCode)));
-    expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.WHITE, Bug.ANT, 0).hashCode)));
-  });
-  
-  group('Coordinate', _coordinate);
-}
+      expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.BLACK, Bug.ANT, 1).hashCode)));
+      expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.WHITE, Bug.QUEEN, 1).hashCode)));
+      expect(new Piece(Player.WHITE, Bug.ANT, 1).hashCode, isNot(equals(new Piece(Player.WHITE, Bug.ANT, 0).hashCode)));
+    });
 
-void _coordinate() {
-  test('equality', () {
-    expect(new Coordinate(20, 100), equals(new Coordinate(20, 100)));
-    expect(new Coordinate(20, 100), isNot(equals(new Coordinate(20, 20))));
-    expect(new Coordinate(20, 100), isNot(equals(new Coordinate(100, 100))));
-  });
-  
-  test('direction', () {
-    expect(new Coordinate(10, 12).direction(new Coordinate(9, 12)), equals(Direction.UP_RIGHT));
-    expect(new Coordinate(9, 12).direction(new Coordinate(8, 12)), equals(Direction.UP_LEFT));
-  });
+    group('Coordinate', _coordinate);
+  }
 
-  test('adjacent even row', () {
-    Coordinate c = new Coordinate(2, 6);
-    var adjacentCoordinates = [
-      new Coordinate(2, 5), // start left, move clockwise
-      new Coordinate(1, 5),
-      new Coordinate(1, 6),
-      new Coordinate(2, 7),
-      new Coordinate(3, 5),
-      new Coordinate(3, 6)
-    ];
-    for (var adjacentCoordinate in adjacentCoordinates) {
-      expect(c.isAdjacent(adjacentCoordinate), isTrue);
-    }
+  static void _coordinate() {
+    test('equality', () {
+      expect(new Coordinate(20, 100), equals(new Coordinate(20, 100)));
+      expect(new Coordinate(20, 100), isNot(equals(new Coordinate(20, 20))));
+      expect(new Coordinate(20, 100), isNot(equals(new Coordinate(100, 100))));
+    });
 
-    var nonAdjacentCoordinates = [
-      new Coordinate(2, 6),
-      new Coordinate(2, 4),
-      new Coordinate(2, 8),
-      new Coordinate(1, 4),
-      new Coordinate(1, 7),
-      new Coordinate(3, 4),
-      new Coordinate(3, 7)
-    ];
-    for (var nonAdjacentCoordinate in nonAdjacentCoordinates) {
-      expect(c.isAdjacent(nonAdjacentCoordinate), isNot(isTrue));
-    }
-  });
+    test('direction', () {
+      expect(new Coordinate(10, 12).direction(new Coordinate(9, 12)), equals(Direction.UP_RIGHT));
+      expect(new Coordinate(9, 12).direction(new Coordinate(8, 12)), equals(Direction.UP_LEFT));
+    });
 
-  test('adjacent odd row', () {
-    Coordinate c = new Coordinate(3, 6);
-    var adjacentCoordinates = [
-      new Coordinate(3, 5), // start left, move clockwise
-      new Coordinate(2, 6),
-      new Coordinate(2, 7),
-      new Coordinate(3, 7),
-      new Coordinate(4, 6),
-      new Coordinate(4, 7)
-    ];
-    for (var adjacentCoordinate in adjacentCoordinates) {
-      expect(c.isAdjacent(adjacentCoordinate), isTrue);
-    }
+    test('adjacent even row', () {
+      Coordinate c = new Coordinate(2, 6);
+      var adjacentCoordinates = [
+        new Coordinate(2, 5), // start left, move clockwise
+        new Coordinate(1, 5),
+        new Coordinate(1, 6),
+        new Coordinate(2, 7),
+        new Coordinate(3, 5),
+        new Coordinate(3, 6)
+      ];
+      for (var adjacentCoordinate in adjacentCoordinates) {
+        expect(c.isAdjacent(adjacentCoordinate), isTrue);
+      }
 
-    var nonAdjacentCoordinates = [
-      new Coordinate(3, 6),
-      new Coordinate(3, 4),
-      new Coordinate(3, 8),
-      new Coordinate(2, 5),
-      new Coordinate(2, 8),
-      new Coordinate(4, 5),
-      new Coordinate(4, 8)
-    ];
-    for (var nonAdjacentCoordinate in nonAdjacentCoordinates) {
-      expect(c.isAdjacent(nonAdjacentCoordinate), isNot(isTrue));
-    }
-  });
+      var nonAdjacentCoordinates = [
+        new Coordinate(2, 6),
+        new Coordinate(2, 4),
+        new Coordinate(2, 8),
+        new Coordinate(1, 4),
+        new Coordinate(1, 7),
+        new Coordinate(3, 4),
+        new Coordinate(3, 7)
+      ];
+      for (var nonAdjacentCoordinate in nonAdjacentCoordinates) {
+        expect(c.isAdjacent(nonAdjacentCoordinate), isNot(isTrue));
+      }
+    });
+
+    test('adjacent odd row', () {
+      Coordinate c = new Coordinate(3, 6);
+      var adjacentCoordinates = [
+        new Coordinate(3, 5), // start left, move clockwise
+        new Coordinate(2, 6),
+        new Coordinate(2, 7),
+        new Coordinate(3, 7),
+        new Coordinate(4, 6),
+        new Coordinate(4, 7)
+      ];
+      for (var adjacentCoordinate in adjacentCoordinates) {
+        expect(c.isAdjacent(adjacentCoordinate), isTrue);
+      }
+
+      var nonAdjacentCoordinates = [
+        new Coordinate(3, 6),
+        new Coordinate(3, 4),
+        new Coordinate(3, 8),
+        new Coordinate(2, 5),
+        new Coordinate(2, 8),
+        new Coordinate(4, 5),
+        new Coordinate(4, 8)
+      ];
+      for (var nonAdjacentCoordinate in nonAdjacentCoordinates) {
+        expect(c.isAdjacent(nonAdjacentCoordinate), isNot(isTrue));
+      }
+    });
+  }
 }
