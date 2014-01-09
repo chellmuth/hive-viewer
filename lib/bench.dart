62009f41d9b9df038b5035d1bd99463a0ebeba71
Chris Hellmuth <chellmuth@gmail.com>
2014-01-08 23:41:52 -0700

    Show static bench with scaled down assets

--- lib/bench.dart
+++ lib/bench.dart
@@ -22,23 +22,41 @@ class Bench {
     context.fill();
     context.stroke();
 
-    _drawPlayerInRect(context, player1, new Rectangle(left, top, width / 2, height));
-    _drawPlayerInRect(context, player2, new Rectangle(left + width / 2, top, width / 2, height));
+    _drawPlayerInRect(context, Player.WHITE, new Rectangle(left, top, width / 2, height));
+    _drawPlayerInRect(context, Player.BLACK, new Rectangle(left + width / 2, top, width / 2, height));
 
     context.restore();
   }
 
-  void _drawPlayerInRect(CanvasRenderingContext2D context, String player, Rectangle bounds) {
+  void _drawPlayerInRect(CanvasRenderingContext2D context, Player player, Rectangle bounds) {
     context.save();
+
+    var name = player == Player.WHITE ? player1 : player2;
 
     var fontSize = 45;
     context.font = '${fontSize}px Futura';
     context.fillStyle = '#000';
 
     var verticalMargin = 10;
-    var metrics = context.measureText(player);
+    var metrics = context.measureText(name);
     var textWidth = metrics.width;
-    context.fillText(player, bounds.left + bounds.width / 2 - textWidth / 2, bounds.top + fontSize + verticalMargin);
+    context.fillText(name, bounds.left + bounds.width / 2 - textWidth / 2, bounds.top + fontSize + verticalMargin);
+
+
+    var totalBugWidth = 0;
+    var assetRatio = 3/4;
+    var bugs = [ Bug.ANT, Bug.GRASSHOPPER, Bug.SPIDER, Bug.BEETLE, Bug.QUEEN ];
+    for (var bug in bugs) {
+      ImageElement asset = AssetLibrary.imageForPiece(new Piece(player, bug, 0));
+      totalBugWidth += asset.naturalWidth * assetRatio;
+    }
+    var xOrigin = (bounds.width - totalBugWidth) / 2 + bounds.left;
+    for (var i = 0; i < 5; i++) {
+      ImageElement asset = AssetLibrary.imageForPiece(new Piece(player, bugs[i], 0));
+      var width = asset.naturalWidth * assetRatio;
+      var height = asset.naturalHeight * assetRatio;
+      context.drawImageScaledFromSource(asset, 0, 0, asset.naturalWidth, asset.naturalHeight, xOrigin + width * i, bounds.bottom - height - verticalMargin, width, height);
+    }
 
     context.restore();
   }
